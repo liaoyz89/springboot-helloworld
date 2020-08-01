4b95a3fb8b0bb125468c0a06365ac9b6fca14d41
liaoyz89 <47731629+liaoyz89@users.noreply.github.com>
2020-08-01 10:12:56 +0800

    Update HomeController.java

--- src/main/java/com/gazgeek/helloworld/controller/HomeController.java
+++ src/main/java/com/gazgeek/helloworld/controller/HomeController.java
@@ -8,7 +8,26 @@ public class HomeController {
 
     @RequestMapping("/")
     String home() {
-        return "Hello from GazGeek!";
+        Long id = this.genRegId("123456");
+        return "Hello from " + id;
+        //return "Hello from GazGeek!";
     }
+    
+    public Long genRegId(String acctNo){
+                logger.debug("----------generateRegId acctNo={}----------", acctNo);
+                DateFormat df = new SimpleDateFormat("yyDS");
+                Calendar c = Calendar.getInstance();
+                int hour = c.get(Calendar.HOUR_OF_DAY);
+                int minute = c.get(Calendar.MINUTE);
+                int second = c.get(Calendar.SECOND);
+                StringBuffer sb = new StringBuffer(acctNo.length() > 7 ?acctNo.substring(acctNo.length()-7):acctNo);
+                sb.append(hour*60*60+minute*60+second);
+
+                sb.append(df.format(c.getTime()).substring(1));
+                String regId = sb.toString();
+                
+                logger.debug("---------生产registerId={}", regId);
+                return Long.parseLong(regId);
+        }
 
 }
